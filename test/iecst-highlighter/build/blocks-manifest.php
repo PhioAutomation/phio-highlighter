87dc18f43b41ca764795583be550a250928408c9
Andy Parman <aparman710@live.com>
2025-08-09 20:07:08 -0700

    Add scaffold Wordpress block

---
+++ test/iecst-highlighter/build/blocks-manifest.php
@@ -0,0 +1,25 @@
+<?php
+// This file is generated. Do not modify it manually.
+return array(
+	'iecst-highlighter' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'create-block/iecst-highlighter',
+		'version' => '0.1.0',
+		'title' => 'IECST Highlighter',
+		'category' => 'widgets',
+		'icon' => 'code',
+		'description' => 'IEC 61131-3 Structured Text Highlighter',
+		'example' => array(
+			
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'textdomain' => 'iecst-highlighter',
+		'editorScript' => 'file:./index.js',
+		'editorStyle' => 'file:./index.css',
+		'style' => 'file:./style-index.css',
+		'viewScript' => 'file:./view.js'
+	)
+);
---
+++ test/iecst-highlighter/build/iecst-highlighter/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'f385b927dc2cd2480df4');
