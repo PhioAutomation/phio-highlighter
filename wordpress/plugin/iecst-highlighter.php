857c534aa35491e99347c22ea60bba8b2610a5c3
Andy Parman <aparman710@live.com>
2025-08-16 12:28:22 -0700

    Adding structure

--- wordpress/plugin/iecst-highlighter.php
+++ wordpress/plugin/iecst-highlighter.php
@@ -3,11 +3,10 @@
  * Plugin Name:       IECST Highlighter
  * Description:       Display beautiful code snippets using the Phio Highlighter for IEC 61131-3 Structured Text
  * Version:           0.1.0
- * Requires at least: 6.7
- * Requires PHP:      7.4
+ * Requires at least: 5.5
+ * Requires PHP:      7.0
  * Author:            Andrew Parman
- * License:           GPL-2.0-or-later
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * License:           MIT
  * Text Domain:       iecst-highlighter
  *
  * @package CreateBlock
@@ -26,36 +25,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 
 function create_block_iecst_highlighter_block_init() {
-	/**
-	 * Registers the block(s) metadata from the `blocks-manifest.php` and registers the block type(s)
-	 * based on the registered block metadata.
-	 * Added in WordPress 6.8 to simplify the block metadata registration process added in WordPress 6.7.
-	 *
-	 * @see https://make.wordpress.org/core/2025/03/13/more-efficient-block-type-registration-in-6-8/
-	 */
-	if ( function_exists( 'wp_register_block_types_from_metadata_collection' ) ) {
-		wp_register_block_types_from_metadata_collection( __DIR__ . '/build', __DIR__ . '/build/blocks-manifest.php' );
-		return;
-	}
-
-	/**
-	 * Registers the block(s) metadata from the `blocks-manifest.php` file.
-	 * Added to WordPress 6.7 to improve the performance of block type registration.
-	 *
-	 * @see https://make.wordpress.org/core/2024/10/17/new-block-type-registration-apis-to-improve-performance-in-wordpress-6-7/
-	 */
-	if ( function_exists( 'wp_register_block_metadata_collection' ) ) {
-		wp_register_block_metadata_collection( __DIR__ . '/build', __DIR__ . '/build/blocks-manifest.php' );
-	}
-	/**
-	 * Registers the block type(s) in the `blocks-manifest.php` file.
-	 *
-	 * @see https://developer.wordpress.org/reference/functions/register_block_type/
-	 */
-	$manifest_data = require __DIR__ . '/build/blocks-manifest.php';
-	foreach ( array_keys( $manifest_data ) as $block_type ) {
-		register_block_type( __DIR__ . "/build/{$block_type}" );
-	}
+	register_block_type( __DIR__ . '/build/iecst-highlighter' );
 }
 add_action( 'init', 'create_block_iecst_highlighter_block_init' );
 
